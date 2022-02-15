7c9c2ddfa59444d11908d28aa5d57f5fbcf9dd0f
Anh Do <anhdotuan.mail@gmail.com>
2022-02-15 00:32:59 -0500

    Added 3rd visual to dashboard

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ import json
 import plotly
 import plotly.express as px
 import plotly.graph_objects as go
-
+import prince
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
@@ -71,7 +72,7 @@ def result():
      
 @app.route("/dashboard")
 def dashboard():
-    return render_template("dashboard.html", title="Dashboard", graphJSON1=plotPie(df), graphJSON2=plotBar(df), feature1=all_features, feature2=all_features)
+    return render_template("dashboard.html", title="Dashboard", graphJSON1=plotPie(df), graphJSON2=plotBar(df), graphJSON3=plotX(df), feature1=all_features, feature2=all_features, feature3=all_features)
 
 @app.route('/cbPie', methods=['GET'])
 def cbPie():
@@ -81,9 +82,9 @@ def cbPie():
 def cbBar():
     return plotBar(df, request.args.get('data'))
 
-@app.route('/callback', methods=['GET'])
+@app.route('/cbX', methods=['GET'])
 def callback():
-    return plotPie(df, request.args.get('data'))
+    return plotX(df, request.args.get('data'))
 
 def plotPie(df, feature="Gender"):
     fig = px.pie(df, names=feature)
@@ -102,6 +103,36 @@ def plotBar(df, feature="Gender"):
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
     
+def plotX(df, features=['Annual Income', 'Occupation','Realty','Car','Phone']):
+    # Prince method - from DOC
+    X=df[features]
+    #mca = prince.MCA(n_components=2, n_iter=3, copy=True, check_input=True, engine='auto',random_state=42)
+    #mca=mca.fit(X)
+    """
+    ax = mca.plot_coordinates(
+                X=X,ax=None, figsize=(6, 6), show_row_points=True, 
+                row_points_size=10, show_row_labels=False, 
+                show_column_points=True, column_points_size=30, show_column_labels=False, legend_n_cols=1)
+    """
+    #fig = ax.get_figure().show()
+
+    """
+    # Prince show with matplotlib
+    fig, ax = plt.subplots()
+    mc = prince.MCA(n_components=2).fit(X)
+    mc.plot_coordinates(X=X, ax=ax)
+    ax.set_xlabel('Component 1', fontsize=16)
+    ax.set_ylabel('Component 2', fontsize=16)
+    plt.show()
+    """
+
+    # Plotly PCA
+    fig = px.scatter_matrix(df, dimensions=features, color="Rejected")
+    fig.update_traces(diagonal_visible=False)
+    
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return graphJSON
 
 if __name__ == "__main__":
     app.run(debug=True)
